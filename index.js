271a8205b4348370c83e7861b6600ff9f453a365
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-19 05:02:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 // Write your solution in this file!
 let driver = {}; 
 
-function updateDriverWithKeyAndValue(driverObject, key, value) {
-  return 
-}
-
-function nondestructivelyUpdateObject(obj, key, value) {
-  const newObj = { ...obj };
- 
-  newObj[key] = value;
- 
-  return newObj;
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriverObject = {...driver}; 
+  newDriverObject[key] = value;
+  return newDriverObject;
 }
